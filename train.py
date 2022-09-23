66efb17dbd9274b84a531334ad69a038205d9123
shaktikanungo <shaktikanungo2019@gmail.com>
2022-09-24 03:02:57 +0530

    added a new function

--- train.py
+++ train.py
@@ -1,3 +1,8 @@
 def say_hello(name):
     print("hello {} , how you doing ? ".format(name))
 say_hello("mallana")
+
+def second_commit(name):
+    print("hello {} , are you learning git ? ".format(name))
+
+second_commit("mallana")
